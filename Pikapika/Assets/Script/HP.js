693c61595969245257e58f972041f987caddda70
Ivy <hkmg038836@gmail.com>
2016-05-03 18:46:26 +0800

    add decrease HP by pressing space

---
+++ Pikapika/Assets/Script/HP.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+private var HP: int = 3;
+var image0: GameObject;
+var image1: GameObject;
+var image2: GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKeyDown("space")){
+    	HP = HP - 1;
+    	print(HP);
+    	image2.gameObject.SetActive(false);
+    	updateUI(HP);
+    }
+
+}
+
+function updateUI(HP){
+	switch(HP){
+		case 2:
+			image2.gameObject.SetActive(false);
+			break;
+		case 1:
+			image1.gameObject.SetActive(false);
+			break;
+		case 0:
+			image0.gameObject.SetActive(false);
+			break;
+	}
+}

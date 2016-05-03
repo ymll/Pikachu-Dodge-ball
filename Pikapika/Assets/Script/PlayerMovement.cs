12a97dda7d6544c71fed7a82b8a88be3662e3784
POONRO <ronniepoon1994@gmail.com>
2016-05-03 20:23:46 +0800

    Trying multiplayer

---
+++ Pikapika/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		this.transform.position = GameObject.Find ("Network Manager").transform.position;
+		GameObject mainCam = GameObject.Find ("Main Camera");
+		mainCam.transform.parent = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

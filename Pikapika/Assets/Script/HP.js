0c4f4ecaf716c376c749c0600182fc01a738cb63
POONRO <ronniepoon1994@gmail.com>
2016-05-04 21:10:02 +0800

    !!

---
+++ Pikapika/Assets/Script/HP.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+private var HP: int = 3;
+var image0: GameObject;
+var image1: GameObject;
+var image2: GameObject;
+var panel: GameObject;
+private var image: GameObject[] ;
+
+function Start () {
+     image = GameObject.FindGameObjectsWithTag("ball");
+}
+
+function Update () {
+	if (Input.GetKeyDown("space")){
+
+    }
+
+}
+function beingHit(){
+    HP = HP - 1;
+	//print(HP);
+	updateUI(HP);
+}
+
+function updateUI(HP){
+	switch(HP){
+		case 2:
+			image[0].gameObject.SetActive(false);
+			break;
+		case 1:
+			image[1].gameObject.SetActive(false);
+			break;
+		case 0:
+			image[2].gameObject.SetActive(false);
+			break;
+	}
+}
--- Pikapika/Assets/Script/PlayerMovement.cs
+++ Pikapika/Assets/Script/PlayerMovement.cs
@@ -5,8 +5,10 @@ public class PlayerMovement : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		this.transform.position = GameObject.Find ("Network Manager").transform.position;
-		GameObject mainCam = GameObject.Find ("Main Camera");
-		mainCam.transform.parent = this.transform;
+		//GameObject mainCam = GameObject.Find ("Main Camera");
+		//mainCam.transform.parent = this.transform;
+		GameObject pikachu = GameObject.Find("P2_Pikachu");
+		pikachu.transform.position = GameObject.Find ("Pikachu").transform.position;
 	}
 	
 	// Update is called once per frame

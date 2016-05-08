a470a787ada70c3e2840302d19214b87199dbcf7
POONRO <ronniepoon1994@gmail.com>
2016-05-09 00:15:16 +0800

    Add class for storing player info and game info

---
+++ Pikapika/Assets/Script/DetectInCourt.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+public class DetectInCourt : MonoBehaviour {
+	float tempX, tempZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		tempX = this.transform.position.x;
+		tempZ = this.transform.position.z;
+
+		if (GameInfo.playerList [int.Parse (this.name)-1].inGame) {
+			//Debug.Log ("Player"+this.name+"is in game now!");
+		}else{	//if not in game
+			// check if the player wanna step in the court
+
+			this.transform.position = new Vector3 (tempX,this.transform.position.y,tempZ);
+		}
+	}
+}
--- Pikapika/Assets/Script/InitializePlayer.cs
+++ Pikapika/Assets/Script/InitializePlayer.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class InitializePlayer : MonoBehaviour {
 	// Use this for initialization
+	int minPlayerToStart = 4; // the min no. of player required to start a game
+	
 	void Start () {
+		GameInfo.numOfPlayer +=1;
+		Debug.Log(GameInfo.numOfPlayer);
+		this.name = GameInfo.numOfPlayer.ToString (); 
 		this.transform.position = GameObject.Find ("Network Manager").transform.position;
+
+		if ((GameInfo.numOfPlayer%2 == 0)&&(GameInfo.numOfPlayer>=minPlayerToStart)){
+			Debug.Log("Game start~");
+			GameInfo.playerList.Add(new PlayerInfo() {playerName=this.name, playerPos=this.transform.position, inGame = true});
+			GameInfo.playerList[int.Parse(this.name)-2].inGame = true;
+		}else{
+			Debug.Log("Needa wait for more players~~");
+			GameInfo.playerList.Add(new PlayerInfo() {playerName=this.name, playerPos=this.transform.position, inGame = false});
+		}
 		//GameObject mainCam = GameObject.Find ("Main Camera");
 		//mainCam.transform.parent = this.transform;
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		GameInfo.playerList [int.Parse(this.name)-1].playerPos = this.transform.position;
 	}
 }

52721dd52e8c8a2a212c4805eac28bf51442f3f2
Lily Yung <ymll@live.hk>
2016-05-15 01:42:08 +0900

    Enable jump and run

--- Pikapika/Assets/Script/Player/PlayerMovement.cs
+++ Pikapika/Assets/Script/Player/PlayerMovement.cs
@@ -4,6 +4,19 @@ using System.Collections;
 
 public class PlayerMovement : NetworkBehaviour {
 
+	public float speed = 7;
+
+	public float gravity = 10;
+
+	private CharacterController controller;
+
+	private Vector3 moveDirection = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController> ();
+	}
+
 	// Update is called once per frame
 	void Update () {
 		if (!isLocalPlayer)
@@ -11,14 +24,34 @@ public class PlayerMovement : NetworkBehaviour {
 			return;
 		}
 
+		ApplyGravity ();
 		ApplyKeyboardControl ();
 	}
 
-	private void ApplyKeyboardControl() {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+	private void ApplyGravity() {
+		if(moveDirection.y > gravity * -1) {
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+		controller.Move (moveDirection * Time.deltaTime);
+	}
 
-		transform.Rotate(0, x, 0);
-		transform.Translate(0, 0, z);
+	private void ApplyKeyboardControl() {
+		bool isRunning = Input.GetKey (KeyCode.LeftShift);
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed * (isRunning ? 2 : 1);
+
+		if (controller.isGrounded) {
+			controller.transform.Rotate (0, x, 0);
+			controller.transform.Translate (0, 0, z);
+
+			if (Input.GetButton ("Jump")) {
+				moveDirection.y = speed;
+			}
+		} else {
+			// Allow move front while jump
+			if (z > 0) {
+				controller.transform.Translate (0, 0, z);
+			}
+		}
 	}
 }

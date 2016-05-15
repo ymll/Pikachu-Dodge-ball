db1b2cf466d98bb779bfb45c4261701c442fd46b
Lily Yung <ymll@live.hk>
2016-05-15 20:04:12 +0900

    Client can throw pokeball

--- Pikapika/Assets/Script/Game/PokeballThrowHandler.cs
+++ Pikapika/Assets/Script/Game/PokeballThrowHandler.cs
@@ -14,32 +14,4 @@ public class PokeballThrowHandler : NetworkBehaviour {
 	void Start () {
 		info = gameObject.GetComponent<PokeballInfo> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonUp(0) && info.isCatchingByPlayer) {
-			CmdThrowPokeball ();
-		}
-	}
-
-	[Command]
-	void CmdThrowPokeball() {
-		float currentTime = Time.time;
-
-		// Player need to hold the ball for a while before throwing it.
-		Debug.Log(currentTime + " - " + info.lastPokeballCatchTime + " < " + minBallHoldingTime + ": " + (currentTime - info.lastPokeballCatchTime < minBallHoldingTime));
-		if (currentTime - info.lastPokeballCatchTime < minBallHoldingTime) {
-			return;
-		}
-
-		info.isCatchingByPlayer = false;
-		info.lastPokeballThrownTime = currentTime;
-
-		GameObject player = NetworkServer.FindLocalObject (info.touchingPlayerId);
-
-		info.touchingPlayerId = new NetworkInstanceId();
-		gameObject.GetComponent<Rigidbody>().isKinematic = false;
-		gameObject.transform.parent = null;
-		gameObject.GetComponent<Rigidbody>().AddForce(player.transform.forward * throwPower, ForceMode.Impulse);
-	}
 }
--- Pikapika/Assets/Script/Player/PlayerMovement.cs
+++ Pikapika/Assets/Script/Player/PlayerMovement.cs
@@ -57,5 +57,56 @@ public class PlayerMovement : NetworkBehaviour {
 				controller.transform.Translate (0, 0, z);
 			}
 		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			throwPokeball ();
+		}
+	}
+
+	private void throwPokeball () {
+		NetworkInstanceId netId = GetComponent<NetworkIdentity> ().netId;
+		CmdThrowPokeball (netId);
+	}
+
+	[Command]
+	void CmdThrowPokeball(NetworkInstanceId netId) {
+		GameObject player = NetworkServer.FindLocalObject (netId);
+		GameObject pokeball = null;
+		foreach (Transform child in player.transform) {
+			if (child.CompareTag ("PlayPokeball")) {
+				pokeball = child.gameObject;
+				break;
+			}
+		}
+		if (pokeball == null) {
+			return;
+		}
+
+		PokeballInfo info = pokeball.GetComponent<PokeballInfo> ();
+
+		// Ignore if command comes from player not catching the ball
+		if (!info.isCatchingByPlayer || info.touchingPlayerId.Value != netId.Value) {
+			return;
+		}
+
+		float minBallHoldingTime = 5;
+
+		float throwPower = 20;
+
+		float currentTime = Time.time;
+
+		// Player need to hold the ball for a while before throwing it.
+		Debug.Log(currentTime + " - " + info.lastPokeballCatchTime + " < " + minBallHoldingTime + ": " + (currentTime - info.lastPokeballCatchTime < minBallHoldingTime));
+		if (currentTime - info.lastPokeballCatchTime < minBallHoldingTime) {
+			return;
+		}
+
+		info.isCatchingByPlayer = false;
+		info.lastPokeballThrownTime = currentTime;
+
+		info.touchingPlayerId = new NetworkInstanceId();
+		pokeball.GetComponent<Rigidbody>().isKinematic = false;
+		pokeball.transform.parent = null;
+		pokeball.GetComponent<Rigidbody>().AddForce(player.transform.forward * throwPower, ForceMode.Impulse);
 	}
 }

f58e6c7d2204f08e929ce2927a83a0fa6c4be370
POONRO <ronniepoon1994@gmail.com>
2016-05-16 02:58:38 +0800

    Mark added not done yet

--- Pikapika/Assets/Script/InitializePlayer.cs
+++ Pikapika/Assets/Script/InitializePlayer.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class InitializePlayer : MonoBehaviour {
 	// Use this for initialization
-	int minPlayerToStart = 4; // the min no. of player required to start a game
-	
+	//int minPlayerToStart = 4; // the min no. of player required to start a game
+	public GameObject playerMark;
+	GameObject mark;
 	void Start () {
+		
 		GameInfo.numOfPlayer +=1;
 		Debug.Log(GameInfo.numOfPlayer);
 		this.name = GameInfo.numOfPlayer.ToString (); 
+		/*
 		this.transform.position = GameObject.Find ("Network Manager").transform.position;
 
 		if ((GameInfo.numOfPlayer%2 == 0)&&(GameInfo.numOfPlayer>=minPlayerToStart)){
@@ -19,10 +22,12 @@ public class InitializePlayer : MonoBehaviour {
 			Debug.Log("Needa wait for more players~~");
 			GameInfo.playerList.Add(new PlayerInfo() {playerName=this.name, playerPos=this.transform.position, inGame = false});
 		}
+		*/
 		//GameObject mainCam = GameObject.Find ("Main Camera");
 		//mainCam.transform.parent = this.transform;
-
-
+		GameInfo.playerList.Add(new PlayerInfo() {playerName=this.name, playerPos=this.transform.position});
+		mark = Instantiate (playerMark, new Vector3(this.transform.position.x,this.transform.position.y+10,this.transform.position.z+1),Quaternion.identity) as GameObject;
+		mark.name = this.name+"Mark" ;
 	}
 	
 	// Update is called once per frame
---
+++ Pikapika/Assets/Script/MinimapChase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinimapChase : MonoBehaviour {
+	string playerNum;
+	// Use this for initialization
+	void Start () {
+		playerNum = this.name.Substring(0,1);
+		Debug.Log (playerNum);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3(GameInfo.playerList [int.Parse (playerNum) - 1].playerPos.x,GameInfo.playerList [int.Parse (playerNum) - 1].playerPos.y+20,GameInfo.playerList [int.Parse (playerNum) - 1].playerPos.z);
+		//this.transform.position = new Vector3 (this.transform.parent.position.x, this.transform.parent.position.y+20, this.transform.parent.position.z+1);
+	}
+}

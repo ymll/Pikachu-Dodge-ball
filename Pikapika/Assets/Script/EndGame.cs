f217cb2e94bfa630154ab285243416581bab1b6c
Ivy <hkmg038836@gmail.com>
2016-05-17 00:54:31 +0800

    hard code gameStarted

---
+++ Pikapika/Assets/Script/EndGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class EndGame : NetworkBehaviour {
+	private GameObject[] win;
+	public GameObject[] playerList;
+	public bool onceStarted;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+		playerList = GameObject.FindGameObjectsWithTag("Player");
+		if (playerList.Length>=2)
+			onceStarted = true;
+		if ((playerList.Length == 1)&&(onceStarted)) {
+			win = GameObject.FindGameObjectsWithTag("win");
+			win[0].gameObject.GetComponent<CanvasGroup>().alpha = 1f;
+		}
+	}
+}
--- Pikapika/Assets/Script/Game/GameStarter.cs
+++ Pikapika/Assets/Script/Game/GameStarter.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 public class GameStarter : NetworkBehaviour {
 
-	private bool isGameStarted;
+	public bool isGameStarted;
 
 	private int connectedPlayerCount;
 

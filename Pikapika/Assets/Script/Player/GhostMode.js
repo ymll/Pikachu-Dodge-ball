cdf0349596b6cbb7b21cc1e997610ccb6f089e69
Lily Yung <ymll@live.hk>
2016-05-17 15:32:53 +0800

    ghost mode no colli

--- Pikapika/Assets/Script/Player/GhostMode.js
+++ Pikapika/Assets/Script/Player/GhostMode.js
@@ -17,7 +17,7 @@ public class GhostMode extends NetworkBehaviour{
 
 		GetComponent(HP).enabled = false;
 		GetComponent(ballPosition).enabled = false;
-		//GetComponent(CharacterController).enabled = false;
+		GetComponent(CharacterController).enabled = false;
 		//transform.localScale = new Vector3(0, 0, 0);
 		(GetComponent("PlayerThrowBallHandler") as NetworkBehaviour).enabled = false;
 		(GetComponent("PlayerCatchBallHandler") as NetworkBehaviour).enabled = false;
